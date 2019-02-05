9941758db78a9266a5a8f7bfbbe80a64347bc5aa
benkuang <bw.kuang@gmail.com>
2019-02-04 21:43:52 -0500

    My first test

---
+++ test/mainTest.js
@@ -0,0 +1,12 @@
+const assert = require('chai').assert;
+
+describe('Testing my link follower', function(){
+
+
+    let x = 10;
+
+    it('Check if variable is a number', function(){
+        assert.isNumber(x);
+    });
+
+});
