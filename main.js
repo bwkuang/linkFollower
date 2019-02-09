e78fd4461e287ca7376cfedd549f10bd9438ef48
benkuang <bw.kuang@gmail.com>
2019-02-09 12:08:08 -0500

    Grab a picture from a web page

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const syncRequest = require('sync-request');
+const fs = require('fs');
 
 function getResponse(url){
     return syncRequest('GET', url); 
@@ -15,5 +16,27 @@ function getResponseBody(url){
     return response.getBody();
 }
 
+function savePictureInSpecifiedFolder(url, folder){
+    var response = getResponse(url);
+    fs.writeFile(folder + '\\mypicture.jpg', response.getBody(), function(err){
+        if(err){
+            console.log(err);
+            return false;
+        }
+        
+    });
+    return true;
+}
+
+function grabLinkOfPicture(urlPaperWithPicture, regExpression){
+    var responseBody = String(getResponse(urlPaperWithPicture).getBody());
+    var linkRegex = new RegExp(regExpression);
+    var linkWithEnvelop = responseBody.match(linkRegex)[0]
+    var link = linkWithEnvelop.match(new RegExp('http.*jpg'));
+    return link;
+}
+
 module.exports.getHttpStatusCode = getHttpStatusCode;
 module.exports.getResponseBody = getResponseBody;
+module.exports.savePictureInSpecifiedFolder = savePictureInSpecifiedFolder;
+module.exports.grabLinkOfPicture = grabLinkOfPicture;
--- test/mainTest.js
+++ test/mainTest.js
@@ -1,30 +1,47 @@
 const assert = require('chai').assert;
 const linkFollower = require('../main.js');
-const regexp = require
+
 
 describe('Testing my link follower', function(){
 
-    var urlVideotron = 'http://www.videotron.com/';
     var urlLapresse = 'https://www.lapresse.ca/';
-    var urlNotFound = 'http://www.videotron.com/idontexist';
+    var urlPicture = 'https://images.lpcdn.ca/641x427/201902/09/1612163-pendant-jours-tas-glace-plus.jpg';
+    var urlPaperWithPicture = 'https://www.lapresse.ca/actualites/grand-montreal/201902/09/01-5214132-longueuil-un-banc-de-glace-dans-une-voie-reservee.php';
 
     it('Should get a successful status code', function(){
-        var responseStatusCode = linkFollower.getHttpStatusCode(urlVideotron);
+        var responseStatusCode = linkFollower.getHttpStatusCode(urlLapresse);
         assert.equal(responseStatusCode, 200);
     });
 
-    it('Should get a "Not found" status code', function(){
-        var responseStatusCode = linkFollower.getHttpStatusCode(urlNotFound);
-        assert.equal(responseStatusCode, 404);
-    });
-    
-    it('Should get the body of videotron web page', function(){
-        var responseBody = linkFollower.getResponseBody(urlVideotron);
-        assert.match(responseBody, new RegExp('.*Services de Téléphonie, Internet, Télévision et Mobile | Vidéotron.*'));
-    });
-
+   
     it('Should get the body of Lapresse web page', function(){
         var responseBody = linkFollower.getResponseBody(urlLapresse);
         assert.match(responseBody, new RegExp('.*Le site d\'information francophone le plus complet en Amérique du Nord: Actualités régionales, provinciales, nationales et internationales.*'));
     });
+
+    it('Should save a picture', function(){
+        var isSaved = linkFollower.savePictureInSpecifiedFolder(urlPicture, 'Z:\\pictureTest');
+        assert.isTrue(isSaved);
+    });
+
+
+    it('Should get the link of a picture based on regex', function(){
+        var regExpression = 'href="https:\/\/images.lpcdn.ca\/.*jpg"';
+        var expectedLink = 'https://images.lpcdn.ca/924x615/201902/09/1612163-pendant-jours-tas-glace-plus.jpg';
+
+        var actualLink = linkFollower.grabLinkOfPicture(urlPaperWithPicture, regExpression);
+        assert.equal(actualLink, expectedLink);
+    });
+
+
+    it('Should get list of links', function(){
+        assert.isTrue(true);
+    });
+
+    // it('Test regex', function(){
+
+    //     var regex = new RegExp('t[abce]st');
+    //     var textToParse = 'Ceci est un test!';
+    //     assert.equal(textToParse.match(regex)[0], 'test');
+    // });
 });
